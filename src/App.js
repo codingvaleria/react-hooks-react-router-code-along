c726bc1bdd26df3c9064799429d75908bdd3a752
codingvaleria <codingvaleria@gmail.com>
2022-09-27 18:58:52 +0300

    Completed assignment

---
+++ src/App.js
@@ -0,0 +1,29 @@
+import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from "./Home";
+import About from "./About";
+import Login from "./Login";
+import NavBar from "./NavBar";
+import Messages from "./Messages";
+import Signup from "./Signup";
+
+function App() {
+    return (
+
+        <React.Fragment>
+            <NavBar />
+            <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/About" element={<About />} />
+                <Route path="/Login" element={<Login />} />
+                <Route path="/Messages" element={<Messages />} />
+                <Route path="/Signup" element={<Signup />} />
+            </Routes>
+        </React.Fragment>
+
+
+
+    );
+}
+
+export default App;
---
+++ src/Messages.js
@@ -0,0 +1,10 @@
+import React from "react";
+function Messages() {
+    return (
+        <div>
+            <h1>This is my message</h1>
+        </div>
+    );
+}
+
+export default Messages;
